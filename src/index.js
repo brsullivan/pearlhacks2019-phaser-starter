64acf757885993307a2378c2aaf8a573d14118c9
Braelyn Sullivan <bsullivan@captechconsulting.com>
2019-02-15 14:19:05 -0500

    Fixes for Presentation
    Updated start point for 2019 presentation date

--- src/index.js
+++ src/index.js
@@ -7,11 +7,10 @@ import alienSpritesheetFile from './assets/images/character_spritesheet.png';
 import musicSfxJsonFile from './assets/audio/music-sfx.json';
 import musicSfxMp3File from './assets/audio/music-sfx.mp3';
 
-
 const config = {
   type: Phaser.AUTO, 
-  height: 900,
-  width: 1200,
+  height: 600,
+  width: 800,
   parent: 'game-container',
   scene: {
     preload: preload,
