51a8a811efc57b4936a7285fd8d1d2eb422d52b8
Braelyn Sullivan <bsullivan@captechconsulting.com>
2019-02-15 20:03:30 -0500

    Update to Input
    Fixed sprite sheet sizing
    Updated powerpoint to Input + Physics section

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import style from './app.css';
 
 import Phaser from 'phaser';
 
+// IMPORT Images + Sprites
 import blockFile from './assets/images/block.png';
 import alienSpritesheetFile from './assets/images/character_spritesheet.png';
+
+// Import AUDIOSPRITE 
 import musicSfxJsonFile from './assets/audio/music-sfx.json';
 import musicSfxMp3File from './assets/audio/music-sfx.mp3';
 
+// SETUP PHASER config 
 const config = {
   type: Phaser.AUTO, 
   height: 600,
@@ -24,14 +28,16 @@ const game = new Phaser.Game(config);
 function preload() {
   this.load.image('block', blockFile);
 
-  this.load.spritesheet('alien', alienSpritesheetFile, { frameWidth: 70, frameHeight: 98 });
+  this.load.spritesheet('alien',
+    alienSpritesheetFile, 
+    { frameWidth: 72, frameHeight: 97 }
+  );
 
   this.load.audioSprite('musicSfx', musicSfxJsonFile, musicSfxMp3File);
 }
 
 function create() {
   this.add.image(200, 200, 'block');
-
   this.add.sprite(400, 300, 'alien');
 
   this.sound.playAudioSprite('musicSfx', 'background-1');
